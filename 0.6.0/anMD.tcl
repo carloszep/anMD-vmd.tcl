dd62a303549e0800ef415bb677bed756c2ae1471
Carlos Z. Gómez Castro <carloszepgc@gmail.com>
2023-03-28 13:10:22 -0600

    bugfixes in loadin libraries with loadLib

--- 0.6.0/anMD.tcl
+++ 0.6.0/anMD.tcl
@@ -104,11 +104,10 @@
 if {![info exists userInfo_version]} {puts "Please, first source 'userInfo' lib"; return}
 
 global anMD_version anMD_logFileName anMD_maxTrajSizeGB anMD_currTrajSize
-global anMD_graphicsOn ind
+global anMD_graphicsOn ind 
 set anMD_version 0.6.0
 set anMD_logFileName "log_anMD-v.$anMD_version.txt"
 set anMD_graphicsOn 1
-
 
 
 # +++ showing introduction +++
