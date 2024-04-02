7a885b618df1d7d0c7294a89384fc7de2ff25d96
Carlos Z. Gómez Castro <carloszepgc@gmail.com>
2024-04-01 21:49:36 -0600

    updated run_vmdAn_xiuhcoatl.sh to launch Slurm jobs in xiuhcoatl

--- temp/run_vmdAn_xiuhcoatl.sh
+++ temp/run_vmdAn_xiuhcoatl.sh
@@ -1,28 +1,54 @@
 #!/bin/bash
+# script for launching vmd analysis jobs in xiuhcoatl (using Slurm)
 
 usage () {
-  echo "usage: run_vmdAn_xiuhcoatl.sh <analysis_script.tcl> <parentDir/workDir> <outDir>"
+  echo "usage: run_vmdAn_xiuhcoatl.sh <analysis_script.tcl> [<parentDir/workDir>] [<outDir>]"
   echo "example: run_vmdAn_xiuhcoatl.sh calc_intEnergy.tcl DHP_mPEG-PCL/micBr2s-s1 intEnergy"
   exit 0
   }
 
+# check for arguments (at least one must be specified)
 if [[ $1 == "" ]]; then usage; fi
 if [ ! -f $1 ]; then usage; fi
-if [[ $2 == "" ]]; then usage; fi
-if [[ $3 == "" ]]; then usage; fi
 
-a=$1
+# arg 1: name of the analysis script
+scriptCalc=$1
+name=${scriptCalc%.tcl}
+workDir=${PWD}
+
+# arg 2: working path for the scratch directory
+if [[ ("$2" == "") || ("$2" == "auto") ]]; then
+  workName="$( basename ${PWD} )/${name}"
+else
+  workName=$2
+fi
+scratchDir="/scratch/users/czgomez/vmdAn/${workName}"
+if [ ! -d ${scratchDir} ]; then
+  echo "creating scratch dir: $scratchDir"
+  mkdir -p ${scratchDir}
+fi
+
+# arg 3: name of the output directory
+if [[ ("$3" == "") || ("$3" == "auto") ]]; then
+  outDir="${name}"
+else
+  outDir=$3
+fi
+if [ ! -d $outDir ]; then
+  echo "creating outDir: $outDir"
+  mkdir $outDir
+fi
 
 sbatch << EOF
 #!/bin/bash
 
-#SBATCH --job-name=${1%.tcl}
+#SBATCH --job-name=${name}
 #SBATCH --output=res.txt
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
 #SBATCH --partition=gpu_k80
 #SBATCH --mail-type=BEGIN,END
-#SBATCH --mail-user=carloszep@gmail.com
+#SBATCH --mail-user=carloszepgc@gmail.com
 #SBATCH --gres=gpu:1
 
 mkdir admon;
@@ -31,37 +57,17 @@ export PATH=\$PATH:/home/apps/gpu/NAMD_2.14_Linux-x86_64-multicore-CUDA/
 export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/home/apps/gpu/NAMD_2.14_Linux-x86_64-multicore-CUDA/lib
 
 
-
-scriptCalc=$1
-workName=$2
-outDir=$3
-name=\${scriptCalc%.tcl}
-workDir=\$PWD
-scratchDir=/scratch/users/czgomez/anMD/\$workName/\$name
-echo "scratchDir: \$scratchDir"
-
-# create directory if it does not exist
-if [ ! -d \${scratchDir} ]; then
-  echo "creating \${scratchDir}"
-  mkdir -p \${scratchDir}
-fi
-
-if [ ! -d \$outDir ]; then
-  echo "creating outDir \$outDir"
-  mkdir \$outDir
-fi
-
 # copying config files
-cp \${workDir}/*.tcl toppar/ \$scratchDir
+cp ${workDir}/*.tcl $scratchDir
 
-cd \$scratchDir
+cd $scratchDir
 
-/home/czgomez/software/vmd/inst/bin/vmd -e \$scriptCalc > job-log_\$name.txt
-#vmd -e \$scriptCalc > job-log_\$name.txt
+/home/czgomez/software/vmd/inst/bin/vmd -dispdev text -e $scriptCalc > job-log_$name.txt
+#vmd -dispdev text -e $scriptCalc > job-log_$name.txt
 
-cp -r \$scratchDir/*.txt \$scratchDir/*.dat \$scratchDir/*.log \$scratchDir/*.csv \$scratchDir/*.tsv \$scratchDir/*.agr \$scratchDir/*.dcd \$scratchDir/*.coor \$scratchDir/*.pdb \$workDir/\$outDir
-#rm -r \$scratchDir/*
-#cp -r \$scratchDir/* \$workDir/\$outDir
+#cp -r $scratchDir/*.txt $scratchDir/*.dat $scratchDir/*.log $scratchDir/*.csv $scratchDir/*.tsv $scratchDir/*.agr $scratchDir/*.dcd $scratchDir/*.coor $scratchDir/*.pdb $workDir/$outDir
+cp -r $scratchDir/* $workDir/${outDir}/
+rm -r $scratchDir/*
 
 echo Termina de Ejecuentar a las: ;
 rm -f mpd.hosts
