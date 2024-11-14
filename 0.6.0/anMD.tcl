dd76cef34cac8c091d193309747c2c49bb170947
Carlos Z. Gómez Castro <carloszepgc@gmail.com>
2024-11-14 17:01:05 -0600

    bugfix for anMD006

--- 0.6.0/anMD.tcl
+++ 0.6.0/anMD.tcl
@@ -103,10 +103,10 @@
 #
 
 # constants, global and default parameters
-if {![info exists userInfo_version]} {puts "Please, first source 'userInfo' lib"; return}
-
 global anMD_version anMD_logFileName anMD_maxTrajSizeGB anMD_currTrajSize
 global anMD_graphicsOn ind 
+if {![info exists userInfo_version]} {puts "Please, first source 'userInfo' lib"; return}
+
 set anMD_version 0.6.0
 set anMD_logFileName "log_anMD-v.$anMD_version.txt"
 set anMD_graphicsOn 0
